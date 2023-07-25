3ca5e07bcb5def3a0e57aa588326c8ce4339d462
litongjava <litongjava@qq.com>
2023-07-25 11:50:05 +0800

    add flag parse

--- main.go
+++ main.go
@@ -13,8 +13,10 @@ import (
 func main() {
 	port := flag.Int("port", 3000, "server port.")
 	dir := flag.String("dir", ".", "wrok path.")
+	flag.Parse()
+
 	addr := ":" + strconv.Itoa(*port)
-	log.Println("litongjava-go-simple-http-server start on", addr, *dir)
+	log.Println("http-server start on", addr, *dir)
 	err := http.ListenAndServe(addr, http.FileServer(http.Dir(*dir)))
 	if err != nil {
 		log.Fatalln(err)
