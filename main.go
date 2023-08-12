ad8871b6f3dbd8877f3465f3bcc6cbab19af44d1
litongjava <litongjava@qq.com>
2023-08-12 18:42:42 +0800

    update to v0.1.3

--- main.go
+++ main.go
@@ -5,20 +5,45 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"strconv"
 )
 
 func main() {
+	var err error
 	port := flag.Int("port", 3000, "server port.")
 	dir := flag.String("dir", ".", "wrok path.")
 	flag.Parse()
 
 	addr := ":" + strconv.Itoa(*port)
+
 	log.Println("http-server start on", addr, *dir)
-	err := http.ListenAndServe(addr, http.FileServer(http.Dir(*dir)))
+	log.Println("Please open the browser to visit the following address:")
+
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		log.Println("Unable to obtain network interface list:", err)
+	}
+	for _, iface := range interfaces {
+		addrs, err := iface.Addrs()
+		if err != nil {
+			log.Println("Unable to obtain IP address list:", err)
+		}
+		for _, addr := range addrs {
+			ipNet, ok := addr.(*net.IPNet)
+			if ok && ipNet.IP.To4() != nil {
+				fmt.Printf("http://%v:%v\n", ipNet.IP, *port)
+			}
+		}
+	}
+
+	err = http.ListenAndServe(addr, http.FileServer(http.Dir(*dir)))
+
 	if err != nil {
 		log.Fatalln(err)
 	}
+
 }
